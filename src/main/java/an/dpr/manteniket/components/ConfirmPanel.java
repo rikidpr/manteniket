125b2540581da02b2051fb00b64a0011adfc579a
rikidpr <saezmr@gmail.com>
2014-10-09 18:51:39 +0200

    alertas bootstrap

--- src/main/java/an/dpr/manteniket/components/ConfirmPanel.java
+++ src/main/java/an/dpr/manteniket/components/ConfirmPanel.java
@@ -1,19 +1,18 @@
 package an.dpr.manteniket.components;
 
 import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.form.Button;
-import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
-import org.apache.wicket.request.mapper.parameter.PageParameters;
+import org.apache.wicket.model.ResourceModel;
 import org.apache.wicket.spring.injection.annot.SpringBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import an.dpr.manteniket.bean.ManteniketContracts;
 import an.dpr.manteniket.dao.ActivitiesDAO;
-import an.dpr.manteniket.domain.Activity;
-import an.dpr.manteniket.template.ManteniketPage;
+import de.agilecoders.wicket.core.markup.html.bootstrap.button.BootstrapButton;
+import de.agilecoders.wicket.core.markup.html.bootstrap.button.Buttons.Type;
+import de.agilecoders.wicket.core.markup.html.bootstrap.dialog.Alert;
+import de.agilecoders.wicket.core.markup.html.bootstrap.form.BootstrapForm;
 
 public class ConfirmPanel extends Panel{
 
@@ -23,17 +22,19 @@ public class ConfirmPanel extends Panel{
     @SpringBean
     private ActivitiesDAO dao;
     //componentes
-    private Label lblConfirmacion;
-    private Button btnAceptar;
-    private Button btnCancelar;
-    private Form form;
+    private Alert lblConfirmacion;
+    private BootstrapButton btnAceptar;
+    private BootstrapButton btnCancelar;
+    private BootstrapForm form;
     
     public ConfirmPanel(String id, String texto, final ConfirmAction actions){
 	super(id);
-	form = new Form("confirmForm");
-	lblConfirmacion = new Label("confirmDetails", Model.of(texto));
+	form = new BootstrapForm("confirmForm");
+//	lblConfirmacion = new Label("confirmDetails", Model.of(texto));
+	lblConfirmacion = new Alert("confirmDetails", Model.of(texto), new ResourceModel("lbl.confirm"));
+	lblConfirmacion.type(Alert.Type.Danger);
 	
-	btnAceptar = new Button("acceptBtn"){
+	btnAceptar = new BootstrapButton("acceptBtn", Type.Warning){
 	    private static final long serialVersionUID = 1L;
 	    @Override
 	    public void onSubmit(){
@@ -42,7 +43,8 @@ public class ConfirmPanel extends Panel{
 	    }
 
 	};
-	btnCancelar = new Button("cancelBtn"){
+	btnAceptar.setLabel(new ResourceModel("btn.accept"));
+	btnCancelar = new BootstrapButton("cancelBtn", Type.Default){
 	    private static final long serialVersionUID = 1L;
 
 	    @Override
@@ -51,6 +53,7 @@ public class ConfirmPanel extends Panel{
 		actions.cancel();
 	    }
 	};
+	btnCancelar.setLabel(new ResourceModel("btn.cancel"));
 	form.add(lblConfirmacion);
 	form.add(btnAceptar);
 	form.add(btnCancelar);
